c43b33dc76a1ee37b37df26f80c0b6182bdd1f25
Quinix Solution <akgbkebu10000@outlook.com>
2022-04-03 21:52:58 +0200

    yes

---
+++ save-points/00-get-started/BlazingPizza.Client/OrderState .cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace BlazingPizza.Client
+{
+    public class OrderState
+    {
+        public Pizza configuringPizza { get; private set; }
+        public bool showingConfigureDialog { get; private set; }
+        public Order Order { get; private set; } = new Order();
+
+        public  void ShowingConfigurePizzaDialog(PizzaSpecial special)
+        {
+            configuringPizza = new Pizza()
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>()
+            };
+            showingConfigureDialog = true;
+        }
+
+        public void CancelConfigurePizzaDialog()
+        {
+            configuringPizza = null;
+            showingConfigureDialog = false;
+        }
+
+        public void ConfirmConfigurePizzaDialog()
+        {
+            Order.Pizzas.Add(configuringPizza);
+            configuringPizza = null;
+
+            showingConfigureDialog = false;
+        }
+
+        public void ResetOrder()
+        {
+            Order = new Order();
+        }
+
+        public void RemoveConfiguredPizza(Pizza pizza)
+        {
+            Order.Pizzas.Remove(pizza);
+        }
+    }
+}
