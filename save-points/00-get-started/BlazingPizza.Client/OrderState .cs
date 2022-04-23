52198f82dfe9c289fa9d7cefaa777f2eb016779f
Quinix Solution <akgbkebu10000@outlook.com>
2022-04-23 17:48:00 +0200

    update

--- save-points/00-get-started/BlazingPizza.Client/OrderState .cs
+++ save-points/00-get-started/BlazingPizza.Client/OrderState .cs
@@ -43,5 +43,10 @@ namespace BlazingPizza.Client
         {
             Order.Pizzas.Remove(pizza);
         }
+
+        public void ReplaceOrder(Order order)
+        {
+            Order = order;
+        }
     }
 }
